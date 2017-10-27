6e988d3aae25a3f0366fd1c4460cdd2190d2f204
Nikola Jandrić <nikola.jandric@outlook.com>
2017-10-27 10:00:38 +0200

    checkInput func added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const noteInputField = document.querySelector('#noteInputField');
 const clearListButton = document.querySelector('#clearListButton');
 const noteList = document.querySelector("#noteList");
 const resultSection = document.querySelector("#resultSection");
+const inputDisplay = document.querySelector("#inputDisplay");
 
 
 
@@ -15,27 +16,42 @@ const resultSection = document.querySelector("#resultSection");
 
 
 
-function addNote (note){
-
+function addNote(note) {
+    if (!checkInput(note)) { return };
+    inputDisplay.textContent = '';
     let newNote = document.createElement("li");
     newNote.textContent = note;
     noteList.appendChild(newNote);
 
 }
 
-resultSection.addEventListener("click", (event)=>{
+resultSection.addEventListener("click", (event) => {
 
-    if (event.target.tagName === "BUTTON"){
+    if (event.target.tagName === "BUTTON") {
         noteList.innerHTML = "";
     }
 })
 
 
-inputSection.addEventListener ("click", (event)=>{
+inputSection.addEventListener("click", (event) => {
 
-    if (event.target.tagName === "BUTTON"){
+    if (event.target.tagName === "BUTTON") {
         addNote(noteInputField.value);
         noteInputField.value = "";
     }
 
 })
+
+
+const checkInput = (note) => {
+    if (note === "") {
+        inputDisplay.textContent = "Warning: empty note!";
+        noteInputField.setAttribute("disabled", "true");
+        window.setTimeout(() => {
+            inputDisplay.textContent = "";
+            noteInputField.removeAttribute("disabled");
+        }, 1000);
+        return false;
+    }
+    return true;
+}
